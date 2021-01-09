9fe80f0be741e121c24ce83277ce313f4362d90e
daytwu <daytwu@gmail.com>
2021-01-08 19:46:49 -0500

    updated

--- plots.js
+++ plots.js
@@ -79,7 +79,24 @@ function buildCharts(sample) {
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
+    var sortedSamples = sample_values.sort((a, b) => a - b);
+    console.log(sortedSamples)
+
+
     // var yticks = 
+
+    // var trace = {
+    //   x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
+    //   y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
+    //   type: "bar"
+    //  };
+    //  var data = [trace];
+    //  var layout = {
+    //   title: "'Bar' Chart",
+    //   xaxis: { title: "Drinks"},
+    //   yaxis: { title: "% of Drinks Ordered"}
+    //  };
+    //  Plotly.newPlot("plotArea", data, layout);
 
     // // 8. Create the trace for the bar chart. 
     // var barData = [
