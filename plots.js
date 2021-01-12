88a234d4f10be524173be6f742fa80cb50756bb7
daytwu <daytwu@gmail.com>
2021-01-12 18:36:37 -0500

    u

--- plots.js
+++ plots.js
@@ -131,7 +131,7 @@ function buildCharts(sample) {
       marker: {
         size: sample_values,
         color: otu_ids,
-        colorscale: "rainbow",
+        colorscale: "Rainbow",
 
       }
 
@@ -154,5 +154,47 @@ function buildCharts(sample) {
     Plotly.newPlot('bubble', bubbleData, bubbleLayout)
 
 
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+
+    // Create a variable that holds the first sample in the array.
+  
+
+    // 2. Create a variable that holds the first sample in the metadata array.
+    
+
+    // Create variables that hold the otu_ids, otu_labels, and sample_values.
+
+
+    // 3. Create a variable that holds the washing frequency.
+    var wfreq = sampleResult.wfreq;
+
+    // Create the yticks for the bar chart.
+    // Hint: Get the the top 10 otu_ids and map them in descending order 
+    // so the otu_ids with the most bacteria are last. 
+    
+    
+    // 4. Create the trace for the gauge chart.
+    
+    gaugeTrace = {
+      domain: {x: [0,1], y: [0,1] },
+      value: wfreq,
+      title: {text: "Bell Button Washing Frequency"},
+      type: "indicator",
+      mode: 'gauge+number'
+      }
+
+    var gaugeData = [
+      gaugeTrace
+    ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      width: 600,
+      height: 500,
+      margin: { t: 0, b: 0 }
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot('myDiv', gaugeData, gaugeLayout)
   });
 }
