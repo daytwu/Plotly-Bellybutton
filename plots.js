02bb9c218432db809fe6e2b6f50d686a08d9be03
daytwu <68725398+daytwu@users.noreply.github.com>
2021-01-12 12:41:11 -0500

    Add files via upload

--- plots.js
+++ plots.js
@@ -93,19 +93,6 @@ function buildCharts(sample) {
 
     var yticks = sorted_ids
 
-    // var trace = {
-    //   x: ["nonalcoholic beer", "nonalcoholic wine", "nonalcoholic martini", "nonalcoholic margarita", "ice tea", "nonalcoholic rum & coke", "nonalcoholic mai tai", "nonalcoholic gin & tonic"],
-    //   y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
-    //   type: "bar"
-    //  };
-    //  var data = [trace];
-    //  var layout = {
-    //   title: "'Bar' Chart",
-    //   xaxis: { title: "Drinks"},
-    //   yaxis: { title: "% of Drinks Ordered"}
-    //  };
-    //  Plotly.newPlot("plotArea", data, layout);
-
     // 8. Create the trace for the bar chart. 
 
     trace = {
@@ -130,6 +117,6 @@ function buildCharts(sample) {
       }
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot('bar', [trace], barLayout)
+    Plotly.newPlot('bar', barData, barLayout)
   });
 }
