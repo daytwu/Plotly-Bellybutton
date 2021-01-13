cdfd0327bb683c67819b2c256ec8017a695425a8
daytwu <daytwu@gmail.com>
2021-01-12 20:57:41 -0500

    i

--- plots.js
+++ plots.js
@@ -188,7 +188,7 @@ function buildCharts(sample) {
             { range: [0, 2], color: 'red'},
             { range: [2, 4], color: 'orange'},
             { range: [4, 6], color: 'yellow'},
-            { range: [6, 8], color: 'lightgreen'},
+            { range: [6, 8], color: 'yellowgreen'},
             { range: [8, 10], color: 'green'}
           ]
         }
